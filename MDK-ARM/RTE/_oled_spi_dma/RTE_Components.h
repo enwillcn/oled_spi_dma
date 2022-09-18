282a4ae2ee1a9822a53edc20bc350220272fb849
enwillcn <56878666+enwillcn@users.noreply.github.com>
2022-09-18 17:37:37 +0800

    OLED + SPI + DMA 使用 SPI+DMA 改善刷屏速度

---
+++ MDK-ARM/RTE/_oled_spi_dma/RTE_Components.h
@@ -0,0 +1,24 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'oled_spi_dma' 
+ * Target:  'oled_spi_dma' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+/*  GorgonMeducer.Performance Counter::Utilities:perf_counter:Core:Library:1.9.8 */
+#define __PERF_COUNTER__
+#define __PERF_COUNTER_CFG_USE_SYSTICK_WRAPPER__
+
+
+#endif /* RTE_COMPONENTS_H */
